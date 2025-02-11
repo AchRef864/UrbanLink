50105e3dfe8a9652c6bac1dde3b1fc187540da90
HeniRidene <heniridene77@gmail.com>
2025-02-11 21:01:11 +0100

    crud avis final !!

--- javacrud/src/main/java/tn/esprit/jdbc/services/AvisService.java
+++ javacrud/src/main/java/tn/esprit/jdbc/services/AvisService.java
@@ -16,61 +16,74 @@ public class AvisService implements CRUD<Avis> {
     @Override
     public int insert(Avis avis) throws SQLException {
         String req = "INSERT INTO `avis`(`note`, `commentaire`, `dateAvis`, `userId`) " +
-                "VALUES ('" + avis.getNote() + "','" + avis.getCommentaire() + "', '" + new java.sql.Date(avis.getDateAvis().getTime()) + "', " + avis.getUserId() + ")";
-
-        st = cnx.createStatement();
-
-        return st.executeUpdate(req);
-    }
-
-    public int insert1(Avis avis) throws SQLException {
-        String req = "INSERT INTO `avis`(`note`, `commentaire`, `dateAvis`, `userId`) VALUES (?, ?, ?, ?)";
-
-        ps = cnx.prepareStatement(req);
-
-        ps.setInt(1, avis.getNote());
-        ps.setString(2, avis.getCommentaire());
-        ps.setDate(3, new java.sql.Date(avis.getDateAvis().getTime()));
-        ps.setInt(4, avis.getUserId());
-
-        return ps.executeUpdate();
+                "VALUES (?, ?, ?, ?)";
+        try {
+            ps = cnx.prepareStatement(req);
+            ps.setInt(1, avis.getNote());
+            ps.setString(2, avis.getCommentaire());
+            ps.setDate(3, new java.sql.Date(avis.getDateAvis().getTime()));
+            ps.setInt(4, avis.getUserId());
+            return ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+        }
     }
 
     @Override
     public int update(Avis avis) throws SQLException {
-        String req = "UPDATE `avis` SET `note` = '" + avis.getNote() + "', `commentaire` = '" + avis.getCommentaire() + "', `dateAvis` = '" + new java.sql.Date(avis.getDateAvis().getTime()) + "', `userId` = " + avis.getUserId() + " WHERE avisId = " + avis.getAvisId();
-        st = cnx.createStatement();
-        return st.executeUpdate(req);
+        String req = "UPDATE `avis` SET `note` = ?, `commentaire` = ?, `dateAvis` = ?, `userId` = ? WHERE `avisId` = ?";
+        try {
+            ps = cnx.prepareStatement(req);
+            ps.setInt(1, avis.getNote());
+            ps.setString(2, avis.getCommentaire());
+            ps.setDate(3, new java.sql.Date(avis.getDateAvis().getTime()));
+            ps.setInt(4, avis.getUserId());
+            ps.setInt(5, avis.getAvisId());
+            return ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+        }
     }
 
     @Override
     public int delete(Avis avis) throws SQLException {
-        String req = "DELETE FROM `avis` WHERE avisId = " + avis.getAvisId();
-        st = cnx.createStatement();
-        return st.executeUpdate(req);
+        String req = "DELETE FROM `avis` WHERE `avisId` = ?";
+        try {
+            ps = cnx.prepareStatement(req);
+            ps.setInt(1, avis.getAvisId());
+            return ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+        }
     }
 
     @Override
     public List<Avis> showAll() throws SQLException {
         List<Avis> temp = new ArrayList<>();
-
         String req = "SELECT * FROM `avis`";
-
-        st = cnx.createStatement();
-
-        ResultSet rs = st.executeQuery(req);
-
-        while (rs.next()) {
-            Avis a = new Avis();
-            a.setAvisId(rs.getInt(1));
-            a.setNote(rs.getInt("note"));
-            a.setCommentaire(rs.getString("commentaire"));
-            a.setDateAvis(rs.getDate("dateAvis"));
-            a.setUserId(rs.getInt("userId"));
-
-            temp.add(a);
+        try {
+            st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            while (rs.next()) {
+                Avis a = new Avis();
+                a.setAvisId(rs.getInt("avisId"));
+                a.setNote(rs.getInt("note"));
+                a.setCommentaire(rs.getString("commentaire"));
+                a.setDateAvis(rs.getDate("dateAvis"));
+                a.setUserId(rs.getInt("userId"));
+                temp.add(a);
+            }
+        } finally {
+            if (st != null) {
+                st.close();
+            }
         }
-
         return temp;
     }
 }
--- javacrud/src/main/java/tn/esprit/jdbc/tests/MainTest.java
+++ javacrud/src/main/java/tn/esprit/jdbc/tests/MainTest.java
@@ -6,34 +6,76 @@ import tn.esprit.jdbc.utils.MyDatabase;
 import java.util.Scanner;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 public class MainTest {
 
     public static void main(String[] args) {
         MyDatabase m1 = MyDatabase.getInstance();
-
-        Avis a1 = new Avis(5, "Great service!", new Date(), 1);
-        Avis a2 = new Avis(4, "Good experience", new Date(), 2);
         Scanner sc = new Scanner(System.in);
         AvisService avisService = new AvisService();
 
         try {
-            //  avisService.insert(a1);
-            //  avisService.insert(a2);
-            //  avisService.delete(a1);
-            System.out.println(avisService.showAll());
+            // Insert initial avis
+            // avisService.insert(a1);
+            // avisService.insert(a2);
+
             System.out.println("Donner votre avis.");
             String commentaire = sc.nextLine();
             System.out.println("Donner la nouvelle note de l'avis.");
             int note = sc.nextInt();
-            a1.setCommentaire(commentaire);
-            a1.setNote(note);
-            avisService.update(a1);
+            sc.nextLine();
+            System.out.println("Donner l'ID de l'utilisateur.");
+            int userId = sc.nextInt();
+            sc.nextLine();
 
-            System.out.println(avisService.showAll());
+            Avis newAvis = new Avis(note, commentaire, new Date(), userId);
+            avisService.insert(newAvis);
+
+            System.out.println("All avis after adding new one:");
+            displayAvisList(avisService.showAll());
+
+
+            System.out.println("Donner l'ID de l'avis à mettre à jour.");
+            int avisIdToUpdate = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Donner le nouveau commentaire.");
+            String newCommentaire = sc.nextLine();
+            System.out.println("Donner la nouvelle note.");
+            int newNote = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Donner le nouvel ID de l'utilisateur.");
+            int newUserId = sc.nextInt();
+            sc.nextLine();
+
+            Avis avisToUpdate = new Avis(newNote, newCommentaire, new Date(), newUserId);
+            avisToUpdate.setAvisId(avisIdToUpdate);
+            avisService.update(avisToUpdate);
+
+            System.out.println("All avis after update:");
+            displayAvisList(avisService.showAll());
+
+            // Delete an avis
+            System.out.println("Donner l'ID de l'avis à supprimer.");
+            int avisIdToDelete = sc.nextInt();
+            sc.nextLine();
+
+            Avis avisToDelete = new Avis();
+            avisToDelete.setAvisId(avisIdToDelete);
+            avisService.delete(avisToDelete);
+
+            System.out.println("All avis after deletion:");
+            displayAvisList(avisService.showAll());
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static void displayAvisList(List<Avis> avisList) {
+        for (Avis avis : avisList) {
+            System.out.println(avis);
+            System.out.println();
         }
     }
 }
